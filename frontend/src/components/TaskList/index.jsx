fd21f86f43646b631320e8513ce0d062ecdb9aea
Tamaki <tranngocbaoduy88@gmail.com>
2020-08-29 23:31:29 +0700

    Upload Interface navigation and icon

---
+++ frontend/src/components/TaskList/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './TaskList.scss'
+TaskList.propTypes = {
+
+};
+
+function TaskList(props) {
+    return (
+        <div className="home-container__task-list" >
+
+        </div>
+    );
+}
+
+export default TaskList;
